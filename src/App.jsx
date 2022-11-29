4431b21f005b76d2fd963fcc5cdee5c49211abc9
Andy Liu <aliu927@gmail.com>
2022-11-28 23:38:00 -0800

    learning react-dnd

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
-import { useState } from "react";
-import Knight from "./component/Knight";
 import "./App.css";
+import Board from "./component/Board";
+import { observe } from "./component/Game";
 /**
  *
  * Make sure to use npm run dev --watch for hot reloads
  *
  */
 function App() {
-  return (
-    <div className="App">
-      <Knight />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import Board from "./component/Board";
+import { observe } from "./component/Game";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// )
+
+const rootElement = document.getElementById("root");
+const root = ReactDOM.createRoot(rootElement);
+
+observe((knightPostion) =>
+  root.render(<Board knightPosition={knightPostion} />)
+);
